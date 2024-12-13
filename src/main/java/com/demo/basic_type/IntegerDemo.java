0333fb3499122f75fadea5c664b31dc4404fabc1
boxin.wu <wuboxin98@qq.com>
2024-12-13 20:53:05 +0800

    Integer.valueOf(null) 会抛出空指针异常

--- src/main/java/com/demo/basic_type/IntegerDemo.java
+++ src/main/java/com/demo/basic_type/IntegerDemo.java
@@ -3,7 +3,16 @@ package com.demo.basic_type;
 public class IntegerDemo {
     public static void main(String[] args) {
 
+        // Integer.valueOf(null) 会抛出空指针异常
+        // System.out.println(Integer.valueOf(null));
 
+        //  Integer.valueOf("") 会抛出数字格式异常
+        // Exception in thread "main" java.lang.NumberFormatException: For input string: ""
+        // 	at java.base/java.lang.NumberFormatException.forInputString(NumberFormatException.java:67)
+        System.out.println(Integer.valueOf(""));
+
+
+        // 包装类型作为参数时，形参的改变不会影响实参
         Integer a = 12;
         System.out.println("main:" + a);
         multipleTwo(a);
