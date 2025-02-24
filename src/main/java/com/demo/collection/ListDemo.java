0435ead3a2233e9e24b7f3ed170d5853025e8c16
boxin.wu <wuboxin98@qq.com>
2025-02-24 17:50:40 +0800

    使用 iterator 的 remove 时，会在原 list 中修改

---
+++ src/main/java/com/demo/collection/ListDemo.java
@@ -0,0 +1,21 @@
+package com.demo.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListDemo {
+
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() % 2 == 0) {
+                iterator.remove();
+            }
+        }
+        System.out.println(list);
+    }
+}
