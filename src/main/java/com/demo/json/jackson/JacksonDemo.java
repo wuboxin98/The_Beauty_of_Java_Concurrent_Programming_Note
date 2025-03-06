6a3d34edb3e4ea6fc80c1126493a6021c0332d4e
boxin.wu <wuboxin98@qq.com>
2025-03-06 20:08:45 +0800

    test

--- src/main/java/com/demo/json/jackson/JacksonDemo.java
+++ src/main/java/com/demo/json/jackson/JacksonDemo.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
+// test
 public class JacksonDemo {
     public static void main(String[] args) throws IOException {
         System.out.println("Jackson Demo");
